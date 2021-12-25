273c0a61f1a6f24cad0b26cd3d0c39d5e6d44fa4
lakshit <lakshitarora14@gmail.com>
2021-12-26 01:27:48 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Explore from './pages/Explore'
-import ForgotPassword from './pages/ForgotPassword'
-import Offers from './pages/Offers'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import PrivateRoute from './components/PrivateRoute'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import Profile from './pages/Profile'
+import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
 import Category from './pages/Category'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+import CreateListing from './pages/CreateListing'
+import EditListing from './pages/EditListing'
+import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -25,11 +29,20 @@ function App() {
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/edit-listing/:listingId' element={<EditListing />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
+
       <ToastContainer />
     </>
   )
 }
+
 export default App
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,64 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { getAuth, updateProfile } from 'firebase/auth'
+import {
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc
+} from 'firebase/firestore'
 import { db } from '../firebase.config'
-import { updateDoc, doc } from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import ListingItem from '../components/ListingItem'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
+
 function Profile() {
   const auth = getAuth()
-  const navigate = useNavigate()
+  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState(null)
   const [changeDetails, setChangeDetails] = useState(false)
-
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
   })
+
   const { name, email } = formData
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingsRef = collection(db, 'listings')
+
+      const q = query(
+        listingsRef,
+        where('userRef', '==', auth.currentUser.uid),
+        orderBy('timestamp', 'desc')
+      )
+
+      const querySnap = await getDocs(q)
+
+      let listings = []
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data()
+        })
+      })
+
+      setListings(listings)
+      setLoading(false)
+    }
+
+    fetchUserListings()
+  }, [auth.currentUser.uid])
 
   const onLogout = () => {
     auth.signOut()
@@ -23,12 +68,12 @@ function Profile() {
   const onSubmit = async () => {
     try {
       if (auth.currentUser.displayName !== name) {
-        //   update display name in firebase
+        // Update display name in fb
         await updateProfile(auth.currentUser, {
           displayName: name
         })
 
-        //   update display name in firestore
+        // Update in firestore
         const userRef = doc(db, 'users', auth.currentUser.uid)
         await updateDoc(userRef, {
           name
@@ -36,34 +81,53 @@ function Profile() {
       }
     } catch (error) {
       console.log(error)
-      toast.error('An error occured')
+      toast.error('Could not update profile details')
     }
   }
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value
     }))
   }
+
+  const onDelete = async (listingId) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await deleteDoc(doc(db, 'listings', listingId))
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingId
+      )
+      setListings(updatedListings)
+      toast.success('Successfully deleted listing')
+    }
+  }
+
+  const onEdit = (listingId) => navigate(`/edit-listing/${listingId}`)
+
   return (
     <div className='profile'>
       <header className='profileHeader'>
         <p className='pageHeader'>My Profile</p>
-        <button className='logOut' type='button' onClick={onLogout}>
+        <button type='button' className='logOut' onClick={onLogout}>
           Logout
         </button>
       </header>
-      <main className='profileDetailHeader'>
-        <p className='profileDetailsText'>Personal Details</p>
-        <p
-          className='changePersonalDetails'
-          onClick={() => {
-            changeDetails && onSubmit()
-            setChangeDetails((prevState) => !prevState)
-          }}
-        >
-          {changeDetails ? 'done' : 'change'}
-        </p>
+
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
+          <p
+            className='changePersonalDetails'
+            onClick={() => {
+              changeDetails && onSubmit()
+              setChangeDetails((prevState) => !prevState)
+            }}
+          >
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+
         <div className='profileCard'>
           <form>
             <input
@@ -84,6 +148,29 @@ function Profile() {
             />
           </form>
         </div>
+
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt='home' />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt='arrow right' />
+        </Link>
+
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className='listingText'>Your Listings</p>
+            <ul className='listingsList'>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   )
